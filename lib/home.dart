254cfedabffdedcffb631fa5e1f95cec91fd647c
VitoriaPereiraKlapowsko <vitoriapereiraklapowsko@gmail.com>
2024-03-24 21:24:02 -0300

    Tornando os botões clicaveis e fazendo a logica do Jokenpo, exibindo os resultados

--- lib/home.dart
+++ lib/home.dart
@@ -1,10 +1,17 @@
-// ignore_for_file: prefer_const_constructors
-
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(title: 'JOKENPO'),
+    ),
+  );
+}
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
@@ -13,6 +20,53 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String resultado = '';
+  String opcaoUsuario = '';
+  String opcaoComputador = '';
+
+  void jogandoPartida(int jogadaUsuario) {
+    final int jogadaComputador = _escolhaComputador();
+    final resultadoFinal = _resultadoFinal(jogadaUsuario, jogadaComputador);
+
+    final jogadaUsuarioTexto = _chancesJogada(jogadaUsuario);
+    final jogadaComputadorTexto = _chancesJogada(jogadaComputador);
+
+    setState(() {
+      resultado = resultadoFinal;
+      opcaoUsuario = 'Você escolheu $jogadaUsuarioTexto';
+      opcaoComputador = 'Computador escolheu $jogadaComputadorTexto';
+    });
+  }
+
+  int _escolhaComputador() {
+    return Random().nextInt(3);
+  }
+
+  String _resultadoFinal(int jogadaUsuario, int jogadaComputador) {
+    if (jogadaUsuario == jogadaComputador) {
+      return 'Ops... Empate!';
+    } else if ((jogadaUsuario == 0 && jogadaComputador == 2) ||
+        (jogadaUsuario == 1 && jogadaComputador == 0) ||
+        (jogadaUsuario == 2 && jogadaComputador == 1)) {
+      return 'Parabéns! Você venceu :)';
+    } else {
+      return 'Computador venceu :(';
+    }
+  }
+
+  String _chancesJogada(int jogada) {
+    switch (jogada) {
+      case 0:
+        return ': Pedra';
+      case 1:
+        return ': Papel';
+      case 2:
+        return ': Tesoura';
+      default:
+        return '';
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,43 +74,71 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Color.fromARGB(255, 194, 177, 29),
         title: Text(
           'JOKENPO',
-          textAlign: TextAlign.center, // Alinha o texto no centro
+          textAlign: TextAlign.center,
         ),
-        centerTitle: true, // Centralizando o título na AppBar
+        centerTitle: true,
       ),
       body: Center(
-        child: Row(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Expanded(
-              child: Image.asset(
-                'assets/images/pedra.png',
-                height: 100, //Definindo as alturas
-                fit: BoxFit.contain, // Ajustando a imagem dentro do contêiner
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () => jogandoPartida(0),
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.zero,
+                  ),
+                  child: Image.asset(
+                    'assets/images/pedra.png',
+                    height: 100,
+                  ),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () => jogandoPartida(1),
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.zero,
+                  ),
+                  child: Image.asset(
+                    'assets/images/papel.png',
+                    height: 100,
+                  ),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () => jogandoPartida(2),
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    padding: EdgeInsets.zero,
+                  ),
+                  child: Image.asset(
+                    'assets/images/tesoura.png',
+                    height: 100,
+                  ),
+                ),
+              ],
             ),
-            Expanded(
-              child: Image.asset(
-                'assets/images/papel.png',
-                height: 100, 
-                fit: BoxFit.contain, 
-              ),
+            SizedBox(height: 20),
+            Text(
+              opcaoUsuario,
+              style: TextStyle(fontSize: 20),
             ),
-            Expanded(
-              child: Image.asset(
-                'assets/images/tesoura.png',
-                height: 100, 
-                fit: BoxFit.contain, 
-              ),
+            Text(
+              opcaoComputador,
+              style: TextStyle(fontSize: 20),
+            ),
+            SizedBox(height: 20),
+            Text(
+              resultado,
+              style: TextStyle(fontSize: 24),
             ),
           ],
         ),
       ),
     );
   }
-}
-
-@override
-Widget build(BuildContext context) {
-  return Container();
 }
